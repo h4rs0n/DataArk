bf01e118d70d7fd6442dc1c1ff5a570023d06263
Harson <youngharson.s@gmail.com>
2025-07-05 17:40:27 +0800

    Feat: Add docker config

--- api/main.go
+++ api/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"EchoArk/api"
 	"EchoArk/common"
+	"fmt"
 )
 
 func main() {
+	display_banner()
 	common.ParseFlag()
 	api.WebStarter(common.DEBUG)
+}
+
+func display_banner() {
+	fmt.Println("  _____     _             _    ____  _  __")
+	fmt.Println(" | ____|___| |__   ___   / \\  |  _ \\| |/ /")
+	fmt.Println(" |  _| / __| '_ \\ / _ \\ / _ \\ | |_) | ' / ")
+	fmt.Println(" | |__| (__| | | | (_) / ___ \\|  _ <| . \\ ")
+	fmt.Println(" |_____\\___|_| |_|\\___/_/   \\_\\_| \\_\\_|\\_\\")
+	fmt.Println("                                          ")
 }
